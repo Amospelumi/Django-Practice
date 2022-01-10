13bef3161979f21f963c0ba76497d79861a5078f
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-10 22:16:49 +0100

    update

--- base/views.py
+++ base/views.py
@@ -4,11 +4,11 @@ from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name': 'Lets learn python!'},
-    {'id':2, 'name': 'Design with me!'},
-    {'id':3, 'name': 'Frontend Developers'}
-]
+# rooms = [
+#     {'id':1, 'name': 'Lets learn python!'},
+#     {'id':2, 'name': 'Design with me!'},
+#     {'id':3, 'name': 'Frontend Developers'}
+# ]
 
 def home(request):
     rooms = Room.objects.all()
@@ -17,9 +17,6 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     contxt = {'room': room}
     return render(request, 'base/room.html', contxt)
