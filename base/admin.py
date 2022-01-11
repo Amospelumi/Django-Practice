57cc147719c30d799f6748394d86c1bcd56d0419
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-11 17:59:31 +0100

    added new models

--- base/admin.py
+++ base/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room
+from .models import Room, Message, Topic
 admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
--- base/models.py
+++ base/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Room(models.Model):
-    # host =
-    # topic =
+
+
+class Topic(models.Model):
     name = models.CharField(max_length=200)
-    # TelephoneNumber = models.IntegerField(max_length=11)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants =
     updated = models.DateTimeField(auto_now=True)
@@ -13,3 +23,15 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.body[0:50]
